75e1fa41ed91ab74b5cf47b374ef19a99ca0ff28
ikram98ai <ikram98ai@gmail.com>
2025-08-29 15:46:48 +0500

    commenting s3 pocily

--- terraform/main.tf
+++ terraform/main.tf
@@ -181,21 +181,21 @@ resource "aws_s3_bucket_public_access_block" "docgram_storage_public_access" {
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_policy" "docgram_storage_policy" {
-  bucket = aws_s3_bucket.docgram_storage.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.docgram_storage.arn}/*"
-      }
-    ]
-  })
-}
+# resource "aws_s3_bucket_policy" "docgram_storage_policy" {
+#   bucket = aws_s3_bucket.docgram_storage.id
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Sid       = "PublicReadGetObject"
+#         Effect    = "Allow"
+#         Principal = "*"
+#         Action    = "s3:GetObject"
+#         Resource  = "${aws_s3_bucket.docgram_storage.arn}/*"
+#       }
+#     ]
+#   })
+# }
 
 # resource "aws_s3_bucket_website_configuration" "docgram_storage_website" {
 #   bucket = aws_s3_bucket.docgram_storage.id
