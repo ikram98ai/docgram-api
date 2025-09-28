e0a0dd195a23f7ea364d77d511c1d95677814b89
ikram98ai <ikram98ai@gmail.com>
2025-09-28 22:15:06 +0500

    fixing module name in output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,24 +1,24 @@
 # App Outputs
 output "api_gateway_url" {
   description = "API Gateway URL"
-  value       = module.app.aws_apigatewayv2_api.docgram_api.api_endpoint
+  value       = module.api.aws_apigatewayv2_api.docgram_api.api_endpoint
 }
 
 output "lambda_function_name" {
   description = "Lambda function name"
-  value       = module.app.aws_lambda_function.docgram_lambda.function_name
+  value       = module.api.aws_lambda_function.docgram_lambda.function_name
 }
 
 output "ecr_repository_url" {
   description = "ECR repository URL"
-  value       = module.app.aws_ecr_repository.docgram_repo.repository_url
+  value       = module.api.aws_ecr_repository.docgram_repo.repository_url
 }
 
 # # ETL
-output "data_lake_bucket_id" {
-  value = module.etl.data_lake_bucket_id
-}
+# output "data_lake_bucket_id" {
+#   value = module.etl.data_lake_bucket_id
+# }
 
-output "scripts_bucket_id" {
-  value = module.etl.scripts_bucket_id
-}
+# output "scripts_bucket_id" {
+#   value = module.etl.scripts_bucket_id
+# }
