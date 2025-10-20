5d9d70b9fb52ce9256ac5eb486a2cb2fa16455cc
ikram98ai <ikram98ai@gmail.com>
2025-10-20 13:36:27 +0500

    adding terraform provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,17 @@
-# terraform {
-#   required_version = ">= 1.0"
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 5.0"
-#     }
-#   }
-# }
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+}
 
 
 module "api" {
